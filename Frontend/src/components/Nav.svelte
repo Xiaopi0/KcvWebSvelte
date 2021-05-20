d98faa780e3e1efa51c2ee10d750acccd7954a30
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-05-20 07:30:37 +0000

    Set NavLinks FontFamily to Times New Roman

--- Frontend/src/components/Nav.svelte
+++ Frontend/src/components/Nav.svelte
@@ -45,13 +45,14 @@
 		text-decoration: none;
 		padding: 1em 0.5em;
 		display: block;
+		font-family: 'Times New Roman';
 	}
 </style>
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">About</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
