39bfe25a641f9d9a7bccb466e5578e86bca8d229
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-05-20 10:25:21 +0000

    Axios not working

---
+++ Frontend/src/routes/@ShadePi/math/index.svelte
@@ -0,0 +1,36 @@
+<script>
+    const axios = require('axios');
+
+    function getOpg() {  
+        
+        axios.get('https://3003-turquoise-mammal-mphszaam.ws-eu04.gitpod.io/opg/FP9_03_12_2019.json')
+        .then(function (response) {
+          // handle success
+          console.log(response);
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function () {
+          // always executed
+        });
+    }
+</script>
+
+<style>
+    h1.Title#MainTitle {
+        font-family: 'Times New Roman';
+    }
+
+    .Container {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+
+<div class="Container">
+    <h1 class="Title" id="MainTitle">@ShadePi/math/index.svelte</h1><br />
+    <button on:click={getOpg()} >Click Me</button>
+</div>
