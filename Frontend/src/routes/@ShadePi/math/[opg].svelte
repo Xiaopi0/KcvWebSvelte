d9fc386755bd4d5cee3007f73b1dd63c8dc50b6a
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-05-25 12:22:22 +0000

    CodeSmashing

--- Frontend/src/routes/@ShadePi/math/[opg].svelte
+++ Frontend/src/routes/@ShadePi/math/[opg].svelte
@@ -14,23 +14,6 @@
 	export let opg;
 
 	const classNames = require('classnames');
-	import { onMount } from 'svelte';
-
-	const setOpgClassName = (_Opg) => {
-		const OpgName = _Opg.opgname
-		const className = classNames({'StartOpg': OpgName.endsWith('a') || OpgName.endsWith('1')}, `${OpgName.replace(".", "_")}`, 'opg')
-		return className;
-	}
-
-	const checkClassName = (_Opg) => {
-		const OpgName = _Opg.opgname
-
-		if(OpgName.endsWith('a') || OpgName.endsWith('1')) {
-			return true
-		} else {
-			return false
-		}
-	}
 </script>
 
 <style>
@@ -52,8 +35,8 @@
 <div class="opgs">
 	<h1 id="mainTitle">{opg}</h1>
 	{#each Opgs as Opg}
-		<span class={setOpgClassName(Opg)} id={Opg.opgname.replace(".", "_")}>
-			{#if checkClassName(Opg)}
+		<span class="{classNames({'StartOpg': Opg.opgname.endsWith('a') || Opg.opgname.endsWith('1')}, `${Opg.opgname.replace(".", "_")}`, 'opg')}" id="{Opg.opgname.replace(".", "_")}">
+			{#if Opg.opgname.endsWith('a') || Opg.opgname.endsWith('1')}
 				<h1>{ Opg.opgname }</h1><br/>
 			{:else}
 				<h2>{ Opg.opgname }</h2><br/>
