098a84f898027bf4fa152b61c5bc05cf6c862002
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-05-27 07:25:28 +0000

    log[inOut]

---
+++ Frontend/src/routes/log[inOut]/_login.svelte
@@ -0,0 +1,32 @@
+<script>
+let email;
+
+async function login(event) {
+    alert(event.target.Email.value)
+}
+</script>
+
+<style>
+    .loginFormContainer, .loginForm {
+        font-family: 'Times New Roman';
+    }
+
+    .formItem {
+        max-width: 30%;
+    }
+</style>
+
+<div class="loginFormContainer">
+    <form class="loginForm" id="LoginForm" on:submit|preventDefault="{login()}">
+        <input class="formItem" type="text" name="Fornavn" placeholder="Fornavn" id="Fornavn" /><br/>
+        <input class="formItem" type="text" name="Efternavn" placeholder="Efternavn" id="Efternavn" /><br/>
+        <input class="formItem" type="text" name="Email" placeholder="Email" id="Email" /><br/>
+        <input class="formItem" type="password" name="Password" placeholder="Password" id="Password" /><br/>
+        <input class="formItem" type="password" name="Confirm_Password" placeholder="Confirm Password" id="Confirm_Password" /><br/>
+    </form>
+    <pre>
+        {#if email}
+            {email}
+        {/if}
+    </pre>
+</div>
---
+++ Frontend/src/routes/log[inOut]/index.svelte
@@ -0,0 +1,39 @@
+<script context="module">
+	export async function preload(page, session) {
+		const { inOut } = page.params;
+
+		return { inOut };
+	}
+</script>
+
+<script>
+    export let inOut
+
+    import Login from './_login.svelte'
+    import Logout from "./_logout.svelte";
+</script>
+
+<style>
+    .pageHolder {
+        font-family: 'Times New Roman';
+        text-align: center;
+    }
+</style>
+
+<div class="pageHolder" id="Main">
+    {#if inOut == 'in'}
+        <span>
+            <h1>Login</h1>
+            <Login></Login>
+        </span>
+    {:else if inOut == 'out'}
+        <span>
+            <h1>Logout</h1>
+            <Logout></Logout>
+        </span>
+    {:else}
+        <span>
+            <h1>Bad Request - Invalid Syntax</h1>
+        </span>
+    {/if}
+</div>
