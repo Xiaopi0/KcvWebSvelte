a0456434f385e283e82cb4739e4a50d62efc6823
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-06-02 11:01:55 +0000

    TinyMCE not working, research

---
+++ Frontend/src/routes/@[user]/[fag]/create[opg].svelte
@@ -0,0 +1,26 @@
+<script context="module">
+    export async function preload(page, session) {
+		const { user, fag, opg  } = page.params;
+
+		return { user, fag, opg };
+	}
+</script>
+
+<script >
+    export let user
+    export let fag
+    export let opg
+
+    //TinyMCE not working, research
+    const tinymce = require('tinymce')
+
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      toolbar_mode: 'floating',
+   });
+</script>
+
+<h1>{ user/fag/opg }</h1>
+
+<textarea></textarea>
